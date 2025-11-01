00f94cb044448808b21faf9c5223056f1157d144
Michael James <mjames63@gatech.edu>
2025-11-01 19:04:15 -0400

    Implement deck shuffle

--- script.js
+++ script.js
@@ -23,3 +23,28 @@ for (const suit of suits) {
         deck.push(`${rank}-${suit}`);
     }
 }
+
+/*  A function to shuffle a deck of cards, uses the Fisher-Yates shuffle
+    [https://bost.ocks.org/mike/shuffle/]
+*/
+function shuffle(array) {
+    // Get the last index in the array
+    let currentIndex = array.length;
+    // Repeat the loop while there are still cards to shuffle, (index decremented each iteraton)
+    while (currentIndex != 0) {
+        /*  Generate a random index
+            Since .random() is between 0 and 1, multiplying by currentIndex will keep the new index
+            within in the range of the array
+            .floor() rounds down to the closest integer
+        */
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        // Swap the elements and current and random
+        let tempElement = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = tempElement;
+    }
+}
+
+// Shuffle the deck
+shuffle(deck);
